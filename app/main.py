1d3e54c4fdc699d1e3d470b3693e82501c2f5e31
eshabaweja <bawejaesha02@gmail.com>
2024-04-21 21:49:50 +0530

    feat: appropriate status codes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, status,HTTPException
 from fastapi.params import Body
 from models.note import Note
 import uuid
@@ -18,7 +18,7 @@ def read_notes():
     return {"data": my_notes}
 
 
-@app.post("/notes")
+@app.post("/notes", status_code=status.HTTP_201_CREATED)
 def create_note(note: Note):
     note.id = str(uuid.uuid4())
     note_dict = note.dict()
@@ -27,13 +27,12 @@ def create_note(note: Note):
 
 
 @app.get("/notes/{id}")
-def read_note(id: str, response: Response):
+def read_note(id: str):
     # print(id)
     for note_dict in my_notes:
         if note_dict["id"] == id:
             return note_dict
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return {"error": f"Note with given id {id} does not exist."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found.")
 
 
 @app.put("/notes/{id}")
